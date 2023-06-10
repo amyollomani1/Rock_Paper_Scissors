68c5c586736b1a92bd3e5ae7e473b17425a5c3b6
amyollomani1 <93606711+amyollomani1@users.noreply.github.com>
2023-06-09 22:03:17 -0400

    Final

---
+++ run.py
@@ -0,0 +1,10 @@
+from client import menu_screen
+
+
+def main():
+    while True:
+        menu_screen()
+    
+if __name__ == "__main__":
+    main()  # This tells compiler to run the main program
+ 
